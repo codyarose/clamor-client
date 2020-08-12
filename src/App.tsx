5e68b03b887cb249e1898bfb620fa4d4ed21674a
codyarose <crose992@gmail.com>
2020-08-12 17:06:02 -0500

    Added AppContext to handle authed state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import jwtDecode from 'jwt-decode'
 
+import { useAppValue } from './util/context'
 import theme from './theme'
 import { GlobalStyles } from './theme/globals'
 import { Home } from './pages/Home'
@@ -10,22 +12,31 @@ import { Signup } from './pages/Signup'
 import Navbar from './components/Navbar'
 import { Container } from './components/common/Container'
 
-function App() {
+const App = () => {
+	const { updateAuthed } = useAppValue()
+
+	const decodedToken = jwtDecode<{ exp: number }>(localStorage.FBIdToken)
+	const authenticated = decodedToken.exp * 1000 > Date.now()
+
+	console.log(authenticated)
+	useEffect(() => {
+		updateAuthed!(authenticated)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
-			<div className="App">
-				<Router>
-					<Navbar />
-					<Container>
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/signup" component={Signup} />
-						</Switch>
-					</Container>
-				</Router>
-			</div>
+			<Router>
+				<Navbar />
+				<Container>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/signup" component={Signup} />
+					</Switch>
+				</Container>
+			</Router>
 		</ThemeProvider>
 	)
 }
