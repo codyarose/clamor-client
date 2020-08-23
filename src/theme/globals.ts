45149563018632399bdedc1844364e91cd7d4611
codyarose <crose992@gmail.com>
2020-08-23 16:34:36 -0500

    Changed base font family

--- src/theme/globals.ts
+++ src/theme/globals.ts
@@ -2,9 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
 	html {
-		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-			"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-			"Helvetica Neue", sans-serif;
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
 	}
 	body {
 		margin: 0;
